eda18e8d8129992af65d01899ed5db5514f1e8a5
TrungKien2705 <nt.kien.2705@gmail.com>
2023-05-15 22:35:09 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  purge: ["./public/**/*.html", "./public/**/*.js"],
   content: [],
   presets: [],
   darkMode: "media", // or 'class'
